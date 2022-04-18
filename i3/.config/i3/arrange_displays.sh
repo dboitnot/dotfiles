90df833d3d7231aecfe2cff264bc7155229a90d4
Dan Boitnott <dboitnot@gmail.com>
2022-04-18 08:33:06 -0500

    Improved i3

---
+++ i3/.config/i3/arrange_displays.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+TOP_LEFT=HDMI-1-3
+TOP_RIGHT=HDMI-1-1
+BOT_LEFT=DisplayPort-2
+BOT_RIGHT=HDMI-A-4
+
+
+xrandr --auto
+xrandr --output $TOP_LEFT --pos 0x0
+xrandr --output $TOP_RIGHT --right-of $TOP_LEFT
+xrandr --output $BOT_LEFT --below $TOP_LEFT
+xrandr --output $BOT_RIGHT --right-of $BOT_LEFT
