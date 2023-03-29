791f6d1be8532fb0ab307b196955f8b4ca082f06
Dan Boitnott <dboitnot@gmail.com>
2023-03-29 15:43:31 -0500

    Improved monitor config

--- i3/.config/i3/arrange_displays.sh
+++ i3/.config/i3/arrange_displays.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-TOP_LEFT=HDMI-1-3
-TOP_RIGHT=HDMI-1-1
-BOT_LEFT=DisplayPort-2
-BOT_RIGHT=HDMI-A-4
+# TOP_LEFT=HDMI-1-3
+# TOP_RIGHT=HDMI-1-1
+# BOT_LEFT=DisplayPort-2
+# BOT_RIGHT=HDMI-A-4
 
 
-xrandr --auto
-xrandr --output $TOP_LEFT --pos 0x0
-xrandr --output $TOP_RIGHT --right-of $TOP_LEFT
-xrandr --output $BOT_LEFT --below $TOP_LEFT
-xrandr --output $BOT_RIGHT --right-of $BOT_LEFT
+# xrandr --auto
+# xrandr --output $TOP_LEFT --pos 0x0
+# xrandr --output $TOP_RIGHT --right-of $TOP_LEFT
+# xrandr --output $BOT_LEFT --below $TOP_LEFT
+# xrandr --output $BOT_RIGHT --right-of $BOT_LEFT
+
+autorandr --change main
